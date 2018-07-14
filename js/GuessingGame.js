b1c9a3f6dc2b9f588e8e3cf8eb82445717daf27c
Josh Parker <josh@Joshs-MacBook-Pro.local>
2018-07-13 17:11:21 -0700

    wip

--- js/GuessingGame.js
+++ js/GuessingGame.js
@@ -1,18 +1,103 @@
 const STATE = Object.freeze({
     IDLE: 1,
-    PLAYING: 2,
-    WIN: 3,
-    LOSE: 4
+    READY: 2,
+    PLAYING: 3,
+    WIN: 4,
+    LOSE: 5
 })
 
 var Game = function() {
-
     this.playersGuess = null;
     this.winningNumber = generateWinningNumber();
+    this.lo = 0;
+    this.hi = 100;
     this.pastGuesses = [];
-    this.status = null;
+    this.status = 'Can you guess a number in 60 seconds?';
     this.state = STATE.IDLE;
+    this.timer = null;
+
+    // this.render();
 }
+
+/****************************
+RENDERING / DOM
+****************************/
+
+// tbd: should prop use stateful handlers instead.
+Game.prototype.render = function() {
+    // event binding
+    $('#play').click((e) => {    
+      this.state = STATE.READY;
+      this.status = 'timer starts when you hit enter';
+      this.update();
+    })    
+    $('#submit').click((e) => {
+       this.makeAGuess(parseInt($('#player-input').val(), 10));
+       $('#player-input').val("");
+       this.update();
+    })
+    $('#reset').click((e) => {
+        this.newGame(game);
+        this.update();
+    })
+    $('#player-input').keypress((e) => {
+        if ( e.which == 13 ) {
+            
+            if (this.state === STATE.READY) {
+                this.state = STATE.PLAYING;
+                //start the clock
+                //this.timer_start = new Date().getTime();
+                this.timer = setInterval(() => {
+                    this.state = (this.state === STATE.PLAYING) ? STATE.LOSE : this.state;
+                    this.makeAGuess(2);
+                }, 5000);
+            }
+            this.makeAGuess(parseInt($('#player-input').val(), 10));
+            $('#player-input').val("");
+            var evt = $.Event('submit');
+            $(window).trigger(evt);
+        }
+        this.update();
+    })
+    this.update();
+}
+
+Game.prototype.update = function() {
+    switch (this.state) {
+        case STATE.IDLE:
+            $('#play').show();
+            $('#player-input').hide();
+            $('#reset').hide();
+            break;
+        case STATE.READY:
+        case STATE.PLAYING:
+            $('#play').hide();
+            $('#player-input').show().focus();
+            $('#reset').hide();
+            $('#lo').text(this.lo);
+            $('#hi').text(this.hi);
+            break;
+        case STATE.WIN:
+            $('#play').hide();
+            $('#player-input').show();
+            $('#player-input').prop({ disabled: false });
+            $('#reset').hide();
+            break;
+        case STATE.LOSE:
+            $('#play').hide();
+            $('#player-input').show();
+            $('#player-input').prop({ disabled: false });
+            $('#reset').hide();
+            break;
+        default:
+            break;
+    }
+    $('#subtitle').text(this.status);
+}
+
+/****************************
+ACCESSORS
+****************************/
 
 Game.prototype.difference = function() {
     return Math.abs(this.playersGuess-this.winningNumber);
@@ -20,77 +105,6 @@ Game.prototype.difference = function() {
 
 Game.prototype.isLower = function() {
     return this.playersGuess < this.winningNumber;
-}
-
-Game.prototype.playersGuessSubmission = function(num) {
-
-    const invalidIf = [
-        (typeof num !== 'number'),
-        (Number.isNaN(num)),
-        (num < 1), 
-        (num > 100)
-    ];
-
-    // validate input
-    if (invalidIf.some(x => x)) {
-        throw "That is an invalid guess.";      
-    } else {
-        this.playersGuess = num;
-        this.status = this.checkGuess();
-    }
-
-    // submit guess
-    return this.status
-}
-
-Game.prototype.setState = function() {
-
-}
-
-Game.prototype.checkGuess = function() {
-
-    const guess = {
-        win:  () => this.playersGuess === this.winningNumber,
-        lose: () => this.pastGuesses.length === 5,
-        dupl: () => this.pastGuesses.includes(this.playersGuess)
-    }
-
-    const response = {
-        lose: "You Lose.",
-        win:  "You Win!",
-        dupl: "You have already guessed that number.",
-        cold: "You\'re ice cold!",
-        cool: "You\'re a bit chilly.",
-        warm: "You\'re lukewarm.",
-        hot:  "You\'re burning up!"
-    }
-
-    // duplicate?
-    if (guess.dupl()) {
-        return response.dupl;  
-    } else {
-        // record guess
-        this.pastGuesses.push(this.playersGuess);
-    }
-
-    // win or lose?
-    if (guess.win()) {
-        this.state = STATE.WIN;
-        return response.win;
-    }
-    if (guess.lose()) {
-        this.state = STATE.LOSE;
-        return response.lose;
-    }
-
-    // how far off?
-    let d = this.difference();
-    if (d < 10) return response.hot;
-    if (d < 25) return response.warm;
-    if (d < 50) return response.cool;
-
-    // must be ice cold.
-    return response.cold;
 }
 
 Game.prototype.provideHint = function() {
@@ -118,6 +132,97 @@ Game.prototype.provideHint = function() {
     })(res);
 }
 
+/****************************
+MUTATORS
+****************************/
+
+Game.prototype.newGame = function() {
+    Game.call(this.game); 
+}
+
+// Game.prototype.makeAGuess = function(guess) {
+//     this.game.playersGuessSubmission(guess);
+// }
+
+Game.prototype.makeAGuess = function(num) {
+
+    const invalidIf = [
+        (typeof num !== 'number'),
+        (Number.isNaN(num)),
+        (num < 1), 
+        (num > 100)
+    ];
+
+    // validate input
+    if (invalidIf.some(x => x)) {
+        throw "That is an invalid guess.";      
+    } else {
+        // submit
+        this.playersGuess = num;
+        this.status = this.checkGuess();
+    }
+}
+
+Game.prototype.checkGuess = function() {
+
+    var g = this.playersGuess;
+
+    const guess = {
+        win:  () => this.playersGuess === this.winningNumber,
+        //lose: () => this.pastGuesses.length === 5,
+        lose: () => this.state === STATE.LOSE,
+        dupl: () => this.pastGuesses.includes(this.playersGuess)
+    }
+
+    const response = {
+        lose: "You Lose.",
+        win:  "You Win!",
+        dupl: "You have already guessed that number.",
+        cold: "You\'re ice cold!",
+        cool: "You\'re a bit chilly.",
+        warm: "You\'re lukewarm.",
+        hot:  "You\'re burning up!"
+    }
+
+    if (guess.lose()) {
+        this.state = STATE.LOSE;
+        return response.lose;
+    }
+
+    // duplicate?
+    if (guess.dupl()) {
+        return response.dupl;  
+    } else {
+        // record guess
+        this.pastGuesses.push(this.playersGuess);
+        // update lo/hi
+        this.lo = this.isLower() ? Math.max(g, this.lo) : this.lo;
+        this.hi = this.isLower() ? this.hi : Math.min(g, this.hi);
+    }
+
+    // win or lose?
+    if (guess.win()) {
+        this.state = STATE.WIN;
+        return response.win;
+    }
+    
+
+    // how far off?
+    let d = this.difference();
+    if (d < 10) return response.hot;
+    if (d < 25) return response.warm;
+    if (d < 50) return response.cool;
+
+    // must be ice cold.
+    return response.cold;
+}
+
+
+
+/****************************
+UTILS
+****************************/
+
 // utils
 function generateWinningNumber() {
     return Math.ceil(Math.random()*100);
@@ -126,43 +231,9 @@ function generateWinningNumber() {
 
 // this is the old UI!
 
-function newGame(game) {
-    Game.call(game); 
-    $('#subtitle').text('Guess a number between 1-100!');
-    $('#guess-list').empty();
-    for (let i=0; i<5; i++) {
-        $('#guess-list').append("<li class='guess'>-</li>");
-    }
-    $('#submit').prop({ disabled: false });
-    $('#hint').prop({ disabled: false });
-}
-
-function makeAGuess(game) {
-    var guess = $('#player-input').val();
-    $('#player-input').val("");
-    var output = game.playersGuessSubmission(parseInt(guess,10));
-    // console.log(output);
-
-    // if (output !== 'You have already guessed that number.') {
-    //     $('#guess-list').find('li:last').remove();
-    //     $('#guess-list').prepend("<li class='guess'>"+game.playersGuess+"</li>");
-    // }
-
-    // if is lower
-    if (game.isLower()) {
-        $('#lo').text(game.playersGuess)
-    } else {
-        $('#hi').text(game.playersGuess)
-    }
 
 
-    $('#subtitle').text(output);
 
-    if (output === 'You Win!' || output === 'You Lose.') {
-        $('#submit').prop({ disabled: true });
-        $('#hint').prop({ disabled: true });
-    } 
-}
 
 // function resetGame(game) {
 //     game = newGame();
--- js/GuessingGame_UI.js
+++ js/GuessingGame_UI.js
@@ -1,180 +1,132 @@
-// var sketch = function(p) {
+var myp5 = new p5( function( p ) {
 
-var radius = 300;
-var xPrev = 0.0;
-var yPrev = 0.0;
-var x = 0.0;
-var y = 0.0;
-var z = 0.0;
-//var step = 0.01; // Size of each step along the path
-var angle = 0;
-var timerAngle = 0;
-var tic_stroke = 0;
-var tic = 0;
+    // track
+    var angle = 0;
+    var radius = 300;
+    // var xPrev = 0.0;
+    // var yPrev = 0.0;
+    var x = 0;
+    var y = 0;
 
-var frame_rate = 30;
-var time_scale = 60; // 60 sec timer.
-var tic_step = (360/time_scale/frame_rate);
+    // timer vars
+    var tic = 0;
+    var timerAngle = 0;
+    var timerRadius = 300 + 300 * (1/10);
 
+    // constants
+    const frame_rate = 30; // 30 frames/sec
+    const time_scale = 60; // 60 sec timer.
+    const tic_step = (360/time_scale/frame_rate); // in degrees
 
+    // game object
+    var game = new Game();
+    var diff_init = game.difference();
 
-var game = new Game();
-var chances = 10;
-var step = (radius * (4/5)) / chances
-var chances_remaining = chances;
+    // config
+    var font;
+    var fontsize = 32;
+    var bg_color;
+    var tic_color;
 
-var font,
-  fontsize = 32
+    p.setup = function () {
 
-function preload() {
-  // Ensure the .ttf or .otf font stored in the assets directory
-  // is loaded before setup() and draw() are called
-  //font = loadFont('assets/SourceSansPro-Regular.otf');
-}
+        bg_color = p.color(235,235,235);
+        tic_color = p.color(0);
+        p.createCanvas(p.windowWidth, p.windowHeight);
+        p.background(bg_color);
+        p.noFill();
+        p.frameRate(frame_rate);
+        p.textFont('Helvetica');
+        p.textSize(fontsize);
+        p.textAlign(p.CENTER, p.CENTER);
 
-function setup() {
-    createCanvas(windowWidth, windowHeight);
-    background(235, 235, 235);
-    noStroke();
-    frameRate(frame_rate);
+        x = p.sin(p.radians(0))*radius;
+        y = p.cos(p.radians(0))*radius;
 
-    // Set text characteristics
-    textFont('Helvetica');
-    textSize(fontsize);
-    textAlign(CENTER, CENTER);
+        //Listen to your custom event
+        $(window).on('submit', function (e) {
+            drawAnnotation();
+        });
 
-    x = sin(radians(0))*radius;
-    y = cos(radians(0))*radius;
+        // go!
+        game.render();
+    }
 
-    diff_init = game.difference();
+    p.windowResized = function() {
+        p.resizeCanvas(p.windowWidth, p.windowHeight);
+    }
 
-
-    // init game state
-    renderState();
-
-    // play
-    $('#play').click(function(e) {    
-      game.state = STATE.PLAYING;
-      renderState();
-    })
-    
-    $('#submit').click(function(e) {
-       makeAGuess(game);
-       chances_remaining--;
-       renderState();
-    })
-    $('#reset').click(function(e) {
-        newGame(game);
-        renderState();
-    })
-
-    $('#player-input').keypress(function(event) {
-        if ( event.which == 13 ) {
-           makeAGuess(game);
-           stampGuess();
-           chances_remaining--;
+    p.draw = function() { 
+        if (game.state === STATE.PLAYING) {
+            drawTrack();
+            drawTimer();
         }
-        renderState();
-    })
-}
+    }
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
+    function drawTrack() {
 
-function draw() { 
-  //background(220);
-  drawViz();
-  drawTimer();
-}
+        var radius, thickness, xPrev, yPrev;
 
-function drawViz() {
+        p.push();
+        p.translate(p.width/2,p.height/2); // center
 
-  radius = 300 - (250 - 250 * (game.difference()/diff_init));
+        radius = 300 - (250 - 250 * (game.difference()/diff_init));
+        thickness = game.difference()/5;
 
-  diff = game.difference()/5;
+        xPrev = x;
+        yPrev = y;
+        x = p.sin(p.radians(angle))*radius;
+        y = p.cos(p.radians(angle))*radius;
+        x2 = p.sin(p.radians(angle))*(radius+thickness);
+        y2 = p.cos(p.radians(angle))*(radius+thickness);
 
-  push();
-  translate(width/2,height/2);// bring zero point to the center 
-  
-  // [xPrev, yPrev] = [x, y];
-  xPrev = x;
-  yPrev = y;
-  x = sin(radians(angle))*radius;
-  y = cos(radians(angle))*radius;
+        p.noFill();
+        p.stroke(0);
+        p.line(x, y, x2, y2);
+        p.line(xPrev, yPrev, x, y);
 
-  x1 = x;
-  y1 = y;
-  x2 = sin(radians(angle))*(radius+diff);
-  y2 = cos(radians(angle))*(radius+diff);
-  
-  //ellipse(x, y, diff);
-  noFill();
-  stroke(0);
+        p.pop();
 
-  line(x1, y1, x2, y2);
-  line(xPrev, yPrev, x, y);
+        angle+= tic_step; // += .5;
+    }
 
-  pop();
-  angle += .5;
-}
+    function drawTimer() {
 
-function drawTimer() {
-  radius = 300 + 300 * (1/chances);
-  tic_size = (tic % 30) ? 5 : 10;
-  tic_stroke = (tic % 3) ? 255 : 0;
+        var tx1, ty1, tx2, ty2, tic_size;
 
-  push();
-  translate(width/2,height/2);// bring zero point to the center 
+        p.push();
+        p.translate(p.width/2,p.height/2); // center
 
-  noFill();
-  stroke(tic_stroke);
-  x1 = sin(radians(timerAngle))*radius;
-  y1 = cos(radians(timerAngle))*radius;
-  x2 = sin(radians(timerAngle))*(radius+tic_size);
-  y2 = cos(radians(timerAngle))*(radius+tic_size);
-  line(x1, y1, x2, y2);
+        tic_size = (tic % 30) ? 5 : 10;
+        tx = p.sin(p.radians(timerAngle))*timerRadius;
+        ty = p.cos(p.radians(timerAngle))*timerRadius;
+        tx2 = p.sin(p.radians(timerAngle))*(timerRadius+tic_size);
+        ty2 = p.cos(p.radians(timerAngle))*(timerRadius+tic_size);
 
-  pop();
+        p.noFill();
+        p.stroke((tic % 3) ? bg_color : tic_color); // only render every 3rd tic
+        p.line(tx, ty, tx2, ty2);
 
-  timerAngle += tic_step;
-  tic++;
-}
+        p.pop();
 
-function stampGuess() {
-  radius = 300 - (250 - 250 * (game.difference()/diff_init));
-  push();
-  translate(width/2,height/2);// bring zero point to the center 
-  fill(0);
-  x = sin(radians(angle))*radius;
-  y = cos(radians(angle))*radius;
-  text(game.playersGuess.toString(), x, y);
-  pop();
-}
+        timerAngle += tic_step;
+        tic++;
+    }
 
-function renderState() {
-  switch (game.state) {
-    case STATE.IDLE:
-      $('#play').show();
-      $('#player-input').hide();
-      $('#reset').hide();
-      break;
-    case STATE.PLAYING:
-      $('#play').hide();
-      $('#player-input').show().focus();
-      $('#reset').hide();
-      break;
-    case STATE.WIN:
-      $('#play').hide();
-      $('#player-input').show();
-      $('#reset').hide();
-      break;
-    case STATE.LOSE:
-      $('#play').hide();
-      $('#player-input').show();
-      $('#reset').hide();
-      break;
-    default:
-      break;
-  }
-}
+    function drawAnnotation() {
+
+        var x, y;
+
+        p.push();
+        p.translate(p.width/2,p.height/2);// bring zero point to the center 
+
+        x = p.sin(p.radians(angle))*radius;
+        y = p.cos(p.radians(angle))*radius;
+
+        p.fill(0);
+        p.text(game.playersGuess.toString(), x, y);
+
+        p.pop();
+    }
+});
+
